23ef2d41b4080473869fb949b5c3bfd87c9e2dd2
Ahmed <ahmedmohamedmahmoud2012005@gmail.com>
2025-07-04 02:30:49 +0300

    ع

--- landing/src/pages/PageNotFound.jsx
+++ landing/src/pages/PageNotFound.jsx
@@ -1,14 +1,9 @@
-import Footer from "@components/ui/Footer";
-import Navbar from "@components/ui/Navbar";
-
 function PageNotFound() {
   return (
     <>
-      <Navbar />
       <div className="flex h-screen items-center justify-center">
         <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
       </div>
-      <Footer />
     </>
   );
 }
